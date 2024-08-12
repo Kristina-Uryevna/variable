7a7fa709d6559a38779a95ccdac9dc62952430f0
Kristina-Uryevna <mironovakristina.ur@gmail.com>
2024-08-12 19:53:53 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,9 @@
+total_tasks = "12"
+number_of_hours = "1.5"
+course_name = "Python"
+time_for_one_lesson = "7.5"
+a = "12"
+b = "1.5"
+c = "Python"
+d = "7.5 минут"
+print(c,",", "всего задач:", a, ",","затрачено часов:", b,",", "среднее время выполнения:", d)
