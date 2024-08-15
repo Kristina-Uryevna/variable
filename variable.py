0030287a7110b76855c4af132de5e48ba4922f0f
Kristina-Uryevna <mironovakristina.ur@gmail.com>
2024-08-15 12:41:41 +0300

    Add files via upload

--- variable.py
+++ variable.py
@@ -1,9 +1,7 @@
-total_tasks = "12"
-number_of_hours = "1.5"
+total_tasks = 12
+number_of_hours = 1.5
 course_name = "Python"
-time_for_one_lesson = "7.5"
-a = "12"
-b = "1.5"
-c = "Python"
-d = "7.5 минут"
-print(c,",", "всего задач:", a, ",","затрачено часов:", b,",", "среднее время выполнения:", d)
+hour = 60
+time_for_one_lesson = (number_of_hours/total_tasks*hour)
+
+print(course_name,",", "всего задач:", total_tasks, ",","затрачено часов:", number_of_hours,",", "среднее время выполнения:", time_for_one_lesson)
